cce84cd118a606ca80bb9cecc82376261781f7d4
Hbk <rainheritier@gmail.com>
2021-05-11 15:46:33 +0200

    HBK branch updated

---
+++ app.js
@@ -0,0 +1,51 @@
+
+function login()
+{
+    signupButton.style.left='500px';
+    loginButton.style.left='45px';
+    mainBtn.style.left='1px';
+    s2.style.color = "#9e1030ff";
+    s2.style.fontSize = "15px";
+    f1.style.color = "";
+    f1.style.fontSize = "";
+
+}
+
+//Signup button movement or animation 
+function signup()
+{
+    mainBtn.style.left="110px";
+    loginButton.style.left='-320px';
+    signupButton.style.left='55px';
+    f1.style.color = "#9e1030ff";
+    f1.style.fontSize = "15px";
+    s2.style.color = "white";
+    s2.style.fontSize = "";
+
+}
+
+var f1 = document.getElementById("f1");
+var s2 = document.getElementById("s2");
+f1.style.transition="1s";
+s2.style.transition="1s";
+console.log(f1.style);
+
+//Top buttons for the signup and signin buttons
+var mainBtn=document.getElementById('move_button');
+//login button ID
+var loginButton=document.getElementById('login');
+//Signup button ID
+var signupButton=document.getElementById('signup');
+
+
+var Log_ = document.getElementById('login_form');
+window.onclick = function(event)
+{
+    if (event.target == Log_)
+    {
+        Log_.style.display = "none";
+    }
+    else{
+        Log_.style.display;
+    }
+}
