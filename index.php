a93fb9eec726e9662495dbe7576257dea54da6f7
Hbk <rainheritier@gmail.com>
2021-05-30 21:00:03 +0200

    forgotPswd

--- index.php
+++ index.php
@@ -1,11 +1,10 @@
 <?php include "header.php"?>
-  
+
 
   <section class="section1">
     <div class="info">
       <h1 class="crimeline" id="crimeline">crime-line</h1>
-      
-      <!-- <img src="images/use-your-voice.gif" alt="use your voice"> -->
+
     </div>
     <div class="img">
       <img src="images/logo.png" alt="" />
@@ -18,10 +17,11 @@
     <div class="info">
       <h1>Hot-lines</h1>
       <p>
-        Reporting crime is a good steppingstone to justice. Don’t be paralyzed by fear, 
-        report injustice and keep our future children, families and friends safe. It Starts 
-        with one phone call. CRIME-LINE connects you with call specialists that are specially 
-        trained professionals, who follow legal procedures ensuring the safest response to each case.
+      Reporting crime is a good first step toward justice. 
+      Don't be paralyzed by fear; report injustice to keep our children,
+      families, and friends safe in the future. It all starts with a phone call.
+      CRIME-LINE connects you with specially trained professionals who follow legal 
+      procedures to ensure the safest response to each case.
       </p>
       <span class="button" id="toWhistle"
         ><a href="#whistle">Call Now !</a></span
@@ -32,14 +32,9 @@
     <div class="info">
       <h1>Community</h1>
       <p>
-        Reply, Post, and Scroll through some of the information to see if you relate
-        with some of the traumas most south Africans go through on daily basis.
-        By engaging with these posts, you can save a life and offer encouragement to 
-        speak up and seek help about your experiences with crime, no matter how small you 
-        think it is, it could be ranging from pickpocketing to violent Crimes. We provide an 
-        open space for you to report and speak out about your experiences. As Crime-Line We 
-        believe Standing together is better than standing alone, UMUNTU NGUMUNTU NGA BANTU
-        (MOTHO KE MOTHO KA BATHO)
+      Reply, Post, and Scroll through our Community page to see if you can relate to some of the daily traumas 
+      that most South Africans face.Crime-Line provides an open space for you to report and speak out about your experiences. 
+      We believe Standing together is better than standing alone. UMUNTU NGUMUNTU NGABANTU (MOTHO KE MOTHO KA BATHO BABANG).
       </p>
       <span class="button" id="toForum"><a href="  forum/">see Community</a></span>
     </div>
@@ -54,12 +49,12 @@
     <div class="info">
       <h1>Get Help</h1>
       <p>
-        The first step to healing is having the will to heal. The desire to feel better can be 
-        your best ally on the road to recovery. Do not give in to the ego, which will try to tell 
-        you there is something wrong with you: there is nothing wrong with you. The reactions you 
-        experience because of trauma are only responses— they are not who you are. Crime-Line’s GET HELP 
-        offers a variety of support groups and legal representation that can be your first step to healing.
-        Crime-Line’s GET HELP offers a variety of support groups and legal representation that can be your first step to healing. 
+      The willingness to heal is the first step toward healing. On the road to recovery, 
+      your desire to feel better can be your most powerful ally. Do not give in to the ego's
+      attempts to convince you that there is something wrong with you: there is nothing wrong with you.
+       The reactions you have as a result of trauma are only reactions; they do not define who you are. 
+       GET HELP from Crime-Line provides a variety of support groups and legal representation 
+       that can be your first step toward healing.
       </p>
       <span class="button" id="toGetHelp"
         ><a href="#getHelp">Get Help</a></span
